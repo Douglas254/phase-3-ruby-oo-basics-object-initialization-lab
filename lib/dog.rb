adab90df97f56c15185af6aec9e8d0eb92451c1d
Douglas Obara <douglasobara97@gmail.com>
2022-08-22 17:52:31 +0300

    complete assignment

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+# Dog
+# #initialize
+# sets the name of the dog in an instance variable @name
+# sets the breed of the dog in an instance variable @breed
+# defaults the breed argument to "Mutt" in an instance variable @breed
+class Dog
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+end
